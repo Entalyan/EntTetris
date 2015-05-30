5b760e434b9682b57998bfbb6aea8b11f7dc5cdd
Entalyan <entalyan@outlook.com>
2015-05-30 10:33:20 +0200

    Builds and runs on Windows 10 (Preview 10130) from Visual Studio 2015 RC

--- Tetris/Game.cs
+++ Tetris/Game.cs
@@ -97,7 +97,7 @@ namespace Tetris
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if ( Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
